f96c4a214d6e6167d76e81933d103d289875d97f
NazwanHilmi <nhr.bzl@gmail.com>
2023-09-28 21:25:41 +0700

    bootstrap cdnjs

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -20,7 +20,7 @@
 <!-- AdminLTE App -->
 <script src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script>
 {{-- Bootstrap 5 --}}
-<script src="{{ asset('bootstrap5/js/bootstrap.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 {{-- FontAwesome --}}
 <script src="{{ asset('plugins') }}/fontawesome/js/all.min.js"></script>
 {{-- Sweet Alert --}}
